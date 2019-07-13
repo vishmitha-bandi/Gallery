ca7cf743ac212e74f4ff59c7ea193208d6d56f3b
vishmitha-bandi <52859766+vishmitha-bandi@users.noreply.github.com>
2019-07-13 20:55:54 +0530

    Update models.py

--- My Gallery/gallery/models.py
+++ My Gallery/gallery/models.py
@@ -1,6 +1,6 @@
 from django.db import models
-#from django.contrib.auth.models import AbstractUser
 
+# Creating a model to maintain data
 class photos(models.Model):
     name=models.CharField( max_length=50)
     image =models.ImageField(upload_to='pics', height_field=None, width_field=None, max_length=None)
