1762ad93981a975102fb799ceb194b0286a7c3d7
vishmitha-bandi <52859766+vishmitha-bandi@users.noreply.github.com>
2019-07-13 20:58:21 +0530

    Update views.py

--- My Gallery/gallery/views.py
+++ My Gallery/gallery/views.py
@@ -1,11 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models import photos
+from .models import photos #importing photos model from models.py
+
+#creating view 
 def home(request):
-    pic= photos.objects.all()
-    #con ={
-     #   'pics':pic
-    #}
+    pic= photos.objects.all() #all objects of photos
     return render(request,"index.html", {'pics':pic})
-    #return HttpResponse("hello")
+  
 
