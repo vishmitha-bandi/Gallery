02df72065be324d0fdcf4991fbc656d05bd3de81
vishmitha-bandi <52859766+vishmitha-bandi@users.noreply.github.com>
2019-07-13 20:08:31 +0530

    Add files via upload

---
+++ My Gallery/myproject/urls.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from django.urls import path,include,re_path
+from django.conf import settings
+from django.conf.urls.static import static
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    re_path ('',include('gallery.urls'))
+]
+urlpatterns =urlpatterns+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+ 
